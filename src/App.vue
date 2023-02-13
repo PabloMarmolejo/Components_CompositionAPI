794dddd445468d1edfe002c4c860645f00e0d77a
Pablo Marmolejo <luismarmolejo79@gmail.com>
2023-02-13 15:39:54 -0600

    First Commit - Definde Props

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+
+import BlogPost from "./components/BlogPost.vue";
+
+const posts = ref([
+    { id: 1, title: "Post 01", body: "Descrión del post 01" },
+    { id: 2, title: "Post 02", body: "Descrión del post 02" },
+    { id: 3, title: "Post 03" },
+]);
+</script>
+
+<template>
+    <div class="container">
+        <h1>Blog</h1>
+        <BlogPost
+            v-for="post in posts"
+            :key="post.title"
+            :title="post.title"
+            :id="post.id"
+            :body="post.body"
+            class="mb-2"
+        />
+    </div>
+</template>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,20 @@
+<script setup>
+// String, Number, Boolean, Array, Object, Date, Function, Symbol
+defineProps({
+  title: String,
+  id: Number,
+  body: {
+      type: String,
+      default: 'Sin descripción'
+  },
+});
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ id }} - {{ title }}</h5>
+      <p>{{ body }}</p>
+    </div>
+  </div>
+</template>
